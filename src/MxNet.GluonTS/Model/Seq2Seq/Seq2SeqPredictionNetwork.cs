88e4896367e47ce68f8d9789103cb80e87cb007c
Deepak Battini <deepakkumar1984@gmail.com>
2020-03-19 23:10:46 +1030

    Gluon TimeSeries project structure completed

---
+++ src/MxNet.GluonTS/Model/Seq2Seq/Seq2SeqPredictionNetwork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxNet.GluonTS.Model.Seq2Seq
+{
+    class Seq2SeqPredictionNetwork
+    {
+    }
+}
---
+++ src/MxNet.GluonTS/Model/Transformer/MultiHeadAttention.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxNet.GluonTS.Model.Transformer
+{
+    class MultiHeadAttention
+    {
+    }
+}
