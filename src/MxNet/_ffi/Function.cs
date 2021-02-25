a9c6eae72bfa89d5569f460276676f78494052f1
deepakkumar1984 <deepakkumar1984@gmail.com>
2021-02-25 17:38:28 +1030

    ....

---
+++ src/MxNet/_ffi/Function.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxNet._ffi
+{
+    public class Function
+    {
+        public IntPtr ObjectHandle;
+
+        public static (MXNetValue[], TypeCode[], int[]) MakeMxnetArgs(List<object> args, List<object> temp_args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FunctionBase : IDisposable
+    {
+        public FunctionBase(IntPtr handle, bool is_global)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MxNet/_ffi/MXNetValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxNet._ffi
+{
+    public class MXNetValue
+    {
+        public Dictionary<string, Type> _fields_ = new Dictionary<string, Type>() {
+            { "v_int64", typeof(long)},
+            { "v_float64", typeof(double) },
+            { "v_handle", typeof(IntPtr)},
+            { "v_str", typeof(string) }
+        };
+
+        public static Dictionary<TypeCode, Type> RETURN_SWITCH = new Dictionary<TypeCode, Type>()
+        {
+            { TypeCode.INT, typeof(long) },
+            { TypeCode.FLOAT, typeof(double) },
+            { TypeCode.NULL, typeof(Nullable) },
+            { TypeCode.NDARRAYHANDLE, typeof(IntPtr) }
+        };
+    }
+}
