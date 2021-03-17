f8b0da4a851b32f47cc106e99619a0d309a46ec9
deepakkumar1984 <deepakkumar1984@gmail.com>
2021-03-17 17:42:34 +1030

    Contrib Text module skeletonized

---
+++ src/MxNet/Contrib/ONNX/Mx2ONNX/OpTranslations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxNet.Contrib.ONNX.Mx2ONNX
+{
+    public class OpTranslations
+    {
+        public static T ParseHelper<T>(Dictionary<string, object> attrs, string attrs_name, T alt_value= default(T))
+        {
+            throw new NotImplementedRelease2Exception();
+        }
+
+        public static int TransformPadding(int pad_width)
+        {
+            throw new NotImplementedRelease2Exception();
+        }
+
+        public static string[] ConvertStringToList(string string_val)
+        {
+            throw new NotImplementedRelease2Exception();
+        }
+
+        public static int GetBooleanAttributeValue(Dictionary<string, object> attrs, string attr_name)
+        {
+            
+            throw new NotImplementedRelease2Exception();
+        }
+    }
+}
